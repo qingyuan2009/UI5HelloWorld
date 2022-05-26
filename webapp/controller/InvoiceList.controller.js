321d37f0e1ef5de983a00cdd37667d1211300964
Rui Zhou <rui.zhou@sap.com>
2022-05-26 15:11:13 +1000

    commit
    commit

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -34,6 +34,33 @@ sap.ui.define([
 			oRouter.navTo("detail", {
 				invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
 			});
-		}
+		},
+		onRemoveRowsModel: function () {
+			var oTable = this.getView().byId("invoiceList");
+			var aSelectedItems = oTable.getSelectedItems();
+
+			for (var i = 0; i < aSelectedItems.length; i++) {			
+				var oItemContextPath = aSelectedItems[i].getBindingContextPath();
+				var aPathParts = oItemContextPath.split("/");
+				var iIndex = aPathParts[aPathParts.length - 1]; //Index to delete into our array of objects
+				var oJSONData = this.getView().getModel("invoice").getData();				
+				oJSONData.Invoices.splice(iIndex, 1); //Use splice to remove your object in the array
+				this.getView().getModel("invoice").setData(oJSONData); //And set the new data to the model
+			}
+			console.log("The removed items are not in the model anymore");
+			console.log(this.getView().getModel("invoice").getData())
+		},
+		onRemoveRowsUi: function(){
+			var oTable = this.getView().byId("invoiceList");
+			var aSelectedItems = oTable.getSelectedItems();
+			
+			for(var i=0; i<aSelectedItems.length; i++){
+			  oTable.removeItem(aSelectedItems[i])
+			}
+			
+			console.log("The removed items are still stored in the model");
+			console.log(this.getView().getModel("invoice").getData())
+		  }
+
 	});
 });
